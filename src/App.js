0479f1cfdd1572bfa277157ec652b76591e32a23
kadrianne <kristine.a.du@gmail.com>
2021-01-10 19:09:00 -0700

    Get restaurants from API and render data in table.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [restaurants, setRestaurants] = useState([]);
+
+    useEffect(() => {
+        fetch('https://code-challenge.spectrumtoolbox.com/api/restaurants', {
+            headers: {
+                Authorization: 'Api-Key q3MNxtfep8Gt',
+            },
+        })
+            .then((response) => response.json())
+            .then(setRestaurants);
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Filter Restaurants</h1>
+            <Table restaurants={restaurants} />
+        </div>
+    );
 }
 
 export default App;
